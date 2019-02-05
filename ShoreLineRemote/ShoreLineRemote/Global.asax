0680c4c82bdb8f36d9f47581d5ce24cf8c023890
BBox11 <31357167+BBox11@users.noreply.github.com>
2019-02-04 20:53:35 -0600

    Added basic search bar, no back end.

---
+++ ShoreLineRemote/ShoreLineRemote/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShoreLineRemote.Global" Language="C#" %>
---
+++ ShoreLineRemote/ShoreLineRemote/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShoreLineRemote.Startup))]
+namespace ShoreLineRemote
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
