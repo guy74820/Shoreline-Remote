3be00b341987edb15c3bf144e2f3b405af1dbee8
BBox11 <31357167+BBox11@users.noreply.github.com>
2019-02-06 14:41:06 -0600

    Created tabs using AJax.

--- ShoreLineRemote/ShoreLineRemote/Default.aspx
+++ ShoreLineRemote/ShoreLineRemote/Default.aspx
@@ -1,12 +1,30 @@
 ﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ShoreLineRemote._Default" %>
 
+<%@ Register assembly="AjaxControlToolkit" namespace="AjaxControlToolkit" tagprefix="ajaxToolkit" %>
+
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
 
-    <div class="jumbotron">
-
-        <asp:TextBox ID="TextBox1" runat="server" Width="253px"></asp:TextBox>
-        <asp:Button ID="Button1" runat="server" Text="Search" />
-
-    </div>
+    <ajaxToolkit:TabContainer ID="ShoreLine" runat="server" ActiveTabIndex="3" Height="336px" Width="904px">
+        <ajaxToolkit:TabPanel ID="TabPanel1" runat="server" HeaderText="TabPanel1">
+            <HeaderTemplate>
+                Overview
+            </HeaderTemplate>
+        </ajaxToolkit:TabPanel>
+        <ajaxToolkit:TabPanel ID="TabPanel2" runat="server" HeaderText="TabPanel2">
+            <HeaderTemplate>
+                Dock<br />
+            </HeaderTemplate>
+        </ajaxToolkit:TabPanel>
+        <ajaxToolkit:TabPanel ID="TabPanel3" runat="server" HeaderText="TabPanel3">
+            <HeaderTemplate>
+                Contact<br />
+            </HeaderTemplate>
+        </ajaxToolkit:TabPanel>
+        <ajaxToolkit:TabPanel ID="TabPanel4" runat="server" HeaderText="TabPanel4">
+            <HeaderTemplate>
+                Create &amp; Search
+            </HeaderTemplate>
+        </ajaxToolkit:TabPanel>
+    </ajaxToolkit:TabContainer>
 
 </asp:Content>
--- ShoreLineRemote/ShoreLineRemote/Default.aspx.designer.cs
+++ ShoreLineRemote/ShoreLineRemote/Default.aspx.designer.cs
@@ -13,21 +13,48 @@ namespace ShoreLineRemote {
     public partial class _Default {
         
         /// <summary>
-        /// TextBox1 control.
+        /// ShoreLine control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        protected global::AjaxControlToolkit.TabContainer ShoreLine;
         
         /// <summary>
-        /// Button1 control.
+        /// TabPanel1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
+        protected global::AjaxControlToolkit.TabPanel TabPanel1;
+        
+        /// <summary>
+        /// TabPanel2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::AjaxControlToolkit.TabPanel TabPanel2;
+        
+        /// <summary>
+        /// TabPanel3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::AjaxControlToolkit.TabPanel TabPanel3;
+        
+        /// <summary>
+        /// TabPanel4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::AjaxControlToolkit.TabPanel TabPanel4;
     }
 }
